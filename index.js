15313abd44448da6a6e69e090ea1af56561c06d0
Rakibur Rahman <rakibur5454@gmail.com>
2021-04-18 16:30:50 +0600

    admin panel updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
-
+const objectID = require('mongodb').ObjectID
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -23,55 +23,80 @@ client.connect(err => {
   const Bookings = client.db("CarService").collection("bookings");
   const AllService = client.db("CarService").collection("AllService");
   const Testimonials = client.db("CarService").collection("Testimonial");
-   
-  app.post('/bookings',(req,res)=>{
-      const BookingData = req.body
-      Bookings.insertOne(BookingData)
-      .then(result =>{
-          console.log(result);
-          res.send(result.insertedCount > 0)
+  const AllAdmin = client.db("CarService").collection("Admin");
+
+  app.post('/bookings', (req, res) => {
+    const BookingData = req.body
+    Bookings.insertOne(BookingData)
+      .then(result => {
+        res.send(result.insertedCount > 0)
       })
 
   })
 
-  app.post('/addService',(req,res)=>{
+  app.post('/addService', (req, res) => {
     const ServiceData = req.body
     AllService.insertOne(ServiceData)
-    .then(result =>{
-      res.send(result)
-    })
-  })
-
-
-  app.post('/testimonial',(req,res)=>{
-      const AllTestimonial = req.body
-      Testimonials.insertOne(AllTestimonial)
-      .then(result =>{
+      .then(result => {
         res.send(result)
-        console.log(result);
       })
   })
 
-  app.get('/allTestimonial',(req,res)=>{
+
+  app.post('/testimonial', (req, res) => {
+    const AllTestimonial = req.body
+    Testimonials.insertOne(AllTestimonial)
+      .then(result => {
+        res.send(result)
+      })
+  })
+
+  app.post('/addAdmin', (req, res) => {
+    const AddAdmin = req.body
+    AllAdmin.insertOne(AddAdmin)
+      .then(result => {
+        res.send(result)
+      })
+  })
+
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email
+    AllAdmin.find({ AdminEmail: email })
+      .toArray((err, admin) => {
+        res.send(admin.length > 0)
+
+      })
+  })
+
+  app.get('/allTestimonial', (req, res) => {
     Testimonials.find({})
-    .toArray((err, testimonial)=>{
-      res.send(testimonial)
-    })
-  })
-
-
-  app.get('/allService',(req,res)=>{
-    AllService.find({})
-    .toArray((err,ServiceData)=>{
-      res.send(ServiceData)
-    })
-  })
-
-  app.get('/bookDetails',(req,res)=>{
-      Bookings.find({})
-      .toArray((err, bookingDetails)=>{
-          res.send(bookingDetails)
+      .toArray((err, testimonial) => {
+        res.send(testimonial)
       })
+  })
+
+
+  app.get('/allService', (req, res) => {
+    AllService.find({})
+      .toArray((err, ServiceData) => {
+        res.send(ServiceData)
+      })
+  })
+
+  app.get('/bookDetails', (req, res) => {
+    Bookings.find({})
+      .toArray((err, bookingDetails) => {
+        res.send(bookingDetails)
+      })
+  })
+
+  app.delete('/deleteService/:id', (req, res) => {
+    const id = objectID(req.params.id)
+    AllService.findOneAndDelete({ _id: id })
+      .then(result => {
+        res.send(!!result.value)
+      })
+
   })
 
 });
